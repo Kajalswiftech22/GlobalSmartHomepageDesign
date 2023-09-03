923d4e3aaa14aecd708701733b9ad9d3044d3d1f
Kajal <acharyakajalswiftech22@gmail.com>
2023-09-03 17:53:16 +0545

    cell

--- Global Smart HomePage Design/Controller/CardViewController.swift
+++ Global Smart HomePage Design/Controller/CardViewController.swift
@@ -11,7 +11,7 @@ struct IconLabelModels {
     var image: String = ""
     var title: String = ""
     var color: UIColor = .clear
-    var type: CellType = .type1
+//    var type: CellType = .type1
 }
 
 enum CellType {
@@ -46,6 +46,11 @@ class CardViewController: UIViewController {
 }
 extension CardViewController: UITableViewDelegate, UITableViewDataSource {
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -53,17 +58,32 @@ extension CardViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let item = iconLabelList[indexPath.row]
-        switch item.type {
-        case .type1:
+        switch indexPath.section {
+        case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "FirstTableViewCell", for: indexPath) as! FirstTableViewCell
             return cell
             
-        case .type2:
+        case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: "SecondTableViewCell", for: indexPath) as! SecondTableViewCell
             cell.configure(model: item)
             return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let firstTableViewCell = "FirstTableViewCell"
+        
+        if firstTableViewCell == "FirstTableViewCell"{
+            return 330
+        }
+        else if firstTableViewCell == "SecondTableViewCell" {
+            return 400
+        }
+        else{
+            return 0
         }
     }
 }
-
-
--- Global Smart HomePage Design/SecondTableViewCell.swift
+++ Global Smart HomePage Design/SecondTableViewCell.swift
@@ -11,12 +11,28 @@ class SecondTableViewCell: UITableViewCell {
     
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+
+    
+    let iconLabelList: [IconLabelModel] = [
+        IconLabelModel(image: "menubar.dock.rectangle.badge.record", title: "Load to", color: .orange),
+        IconLabelModel(image: "creditcard.fill", title: "Merchant", color: .systemMint),
+        IconLabelModel(image: "iphone", title: "TopUp", color: .systemGray5),
+        IconLabelModel(image: "iphone.gen2.radiowaves.left.and.right", title: "Data", color: .systemGray5),
+        IconLabelModel(image: "wifi", title: "ISP", color: .systemMint),
+        IconLabelModel(image: "creditcard.circle", title: "Credit Card", color: .systemCyan),
+        IconLabelModel(image: "car.rear.waves.up.fill", title: "EMI", color: .systemGray5),
+        IconLabelModel(image: "beach.umbrella.fill", title: "Insurance", color: .systemCyan)
+    ]
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
+//        flowLayout.minimumInteritemSpacing = 10
+//        flowLayout.minimumLineSpacing = 10
+        
         collectionView.register(UINib(nibName: "iconsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "iconsCollectionViewCell")
-
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,22 +45,27 @@ class SecondTableViewCell: UITableViewCell {
         collectionView.delegate = self
         collectionView.backgroundColor = .white
     }
-    
 }
 
 extension SecondTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return iconLabelList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "iconsCollectionViewCell", for: indexPath) as! CollectionViewCell
         
-        //        cell.configure(model: iconLabelList[indexPath.row])
+        cell.configure(model: iconLabelList[indexPath.row])
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 10, height: 10)
+        let collectionViewWidth = collectionView.frame.width
+        let numberOfItemsPerRow: CGFloat = 4
+
+        let cellWidth = (collectionViewWidth - (numberOfItemsPerRow - 1) * flowLayout.minimumInteritemSpacing) / numberOfItemsPerRow
+
+        return CGSize(width: cellWidth, height: 100)
     }
 }
